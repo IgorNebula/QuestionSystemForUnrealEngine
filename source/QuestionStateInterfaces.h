ea9a597cd374806fe9c5f98c3472959dc4159ffa
NorthIgor-dev <77532282+NorthIgor-dev@users.noreply.github.com>
2025-05-25 10:42:42 +0300

    new file:   source/QuestionStateInterfaces.h 	modified:   .gitignore

---
+++ source/QuestionStateInterfaces.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+/*
+Базовый интерфейс для состояний
+ */
+
+class IQuestState
+{
+};
+
+/*
+ Интерфейс для передачи сообщений между ветвями
+*/
+
+class IsignalHandler
+{
+};
